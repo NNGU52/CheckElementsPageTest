426b3d9e0d29845724abe13eecc9a6834f9fe54a
elena <galkinaelenann52@gmail.com>
2023-01-30 22:51:44 +0300

    Initial commit

---
+++ CheckElementsPageTest/BaseClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Threading;
+using OpenQA.Selenium.Interactions;
+
+
+namespace CheckElementsPageTest
+{
+    class BaseClass
+    {
+        protected IWebDriver driver;
+
+        [OneTimeSetUp]
+        protected void DoBeforeAllTheTests()
+        {
+            
+        }
+
+        [OneTimeTearDown]
+        protected void DoAfterAllTheTests()
+        {
+            
+        }
+
+        [SetUp]
+        protected void DoBeforeEach()
+        {
+
+        }
+
+        [TearDown]
+        protected void DoAfterEach()
+        {
+            
+        }
+
+    }
+}
---
+++ CheckElementsPageTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Threading;
+using OpenQA.Selenium.Interactions;
+using System;
+
+namespace CheckElementsPageTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
